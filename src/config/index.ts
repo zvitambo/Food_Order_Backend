19673c07e8ca5396fdaf24cdb5ddbf6e3704409e
Thomas Jindu <thomas@spectrumits.co.zw>
2022-07-12 12:57:54 +0200

    added mondb cloud connection

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,7 @@
-export const MONGO_URI = "mongodb://localhost/online_food_delivery";
-//mongodb+srv://tambo:<password>@cluster0.m2lbb.mongodb.net/?retryWrites=true&w=majority
+export const MONGO_URI =
+  "mongodb+srv://tambo:<Nataliee_2412>@cluster0.m2lbb.mongodb.net/<online_food_delivery>?retryWrites=true&w=majority";
+//"mongodb://localhost/online_food_delivery";
+//mongodb+srv://tambo:<Nataliee_2412>@cluster0.m2lbb.mongodb.net/<online_food_delivery>?retryWrites=true&w=majority
 export const APP_SECRET = "my_secret-code_here";
+
+export const PORT = process.env.PORT || 3000;
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 import express from 'express';
 import App from './services/ExpressApp';
 import dbConnection from './services/Database';
+import { PORT } from './config'; 
 
 
 const StartServer = async() => {
@@ -12,8 +13,8 @@ const StartServer = async() => {
      
      await App(app);
 
-     app.listen(3000, () => {
-       console.log("Server started on port 3000");
+     app.listen(PORT, () => {
+       console.log(`Server started on port ${PORT}`);
      }); 
 }
 
