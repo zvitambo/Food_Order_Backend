196725dc6fa3b2f6bf23f28073d61e565dc4c4c3
Thomas Jindu <thomas@spectrumits.co.zw>
2022-07-12 13:11:35 +0200

    added mondb cloud connection with gitignore file update 02

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
-export const MONGO_URI =
+export const MONGO_URI = 
   "mongodb+srv://tambo:<Nataliee_2412>@cluster0.m2lbb.mongodb.net/<online_food_delivery>?retryWrites=true&w=majority";
 //"mongodb://localhost/online_food_delivery";
 //mongodb+srv://tambo:<Nataliee_2412>@cluster0.m2lbb.mongodb.net/<online_food_delivery>?retryWrites=true&w=majority
